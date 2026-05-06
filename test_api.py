576b40c2eac52d078a416ff6b0cb048b4cda696a
huang1hong2wei3 <1761433469@qq.com>
2026-05-06 20:13:42 +0800

    chore: 移除测试相关临时文件，精简仓库

--- test_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-try:
-    response = requests.post("http://localhost:8000/chat", json={"message": "北京天气"})
-    print("Status code:", response.status_code)
-    print("Response:", response.json())
-except Exception as e:
-    print("Error:", e)
